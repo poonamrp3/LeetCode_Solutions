4ad1b2cbedee5131c7723da018477d63bc5371fb
Poonam Pawar <96135472+poonamrp3@users.noreply.github.com>
2024-02-15 12:58:29 -0500

    Time: 20 ms (14.14%) | Memory: 26.6 MB (8.46%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -3,18 +3,29 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int, int> mpp;
-        int threshold = nums.size() / 2;
-
-        for(int i = 0; i < nums.size(); i++){
-            mpp[nums[i]]++;
+       int el;
+       int cnt = 0, n = nums.size();
+       for(int i = 0; i < n ; i++){
+           if(cnt == 0){
+               el = nums[i];
+               cnt = 1;
+           } else if(nums[i] == el){
+               cnt++;
+           } else {
+               cnt--;
+           }
+       }
+       
+        int cnt1 =0;
+        for(int i = 0; i < n ; i++){
+           if(nums[i] == el){
+               cnt1++;
+           }
         }
-
-        for(const auto& pair:mpp){
-            if(pair.second > threshold){
-                return pair.first;
-            }
-        }	
-        return 0;
+        
+        if(cnt1 > n / 2){
+            return el;
+        }
+        return -1;
     }
 };
